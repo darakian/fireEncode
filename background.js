821403c82401172d39b3ec90b4e203f8c30134bd
Jon Moroney <j.moroney@cs.ru.nl>
2017-10-06 17:52:50 +0200

    Working on CORS issue

--- background.js
+++ background.js
@@ -10,8 +10,20 @@ if (info.menuItemId == "fireEncode-image") {
   if(info.mediaType=="image" ){
     var reader  = new FileReader();
     var basedImage = "";
+    var fetchHeaders = new Headers();
+    fetchHeaders.set('Accept', 'image/*');
+    var fetchInit = { method: 'GET',
+               headers: fetchHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+
 
     reader.addEventListener("load", () => {
+      browser.sidebarAction.setPanel(
+        data:text/plain, reader.result  // object
+      )
+
       browser.notifications.create({
         "type": "basic",
         //"iconUrl": browser.extension.getURL("icons/encode.png"),
@@ -20,12 +32,13 @@ if (info.menuItemId == "fireEncode-image") {
         });
     });
 
-    fetch(info.srcUrl)
+    fetch(info.srcUr, fetchInit)
     .then((response) => response.blob())
     .then((blob) => reader.readAsDataURL(blob));
-
-
-
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+      console.log('There has been a problem with your fetch operation: ' + info.srcUrl);
+    });
     }
   }
 });
